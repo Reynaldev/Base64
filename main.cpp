43936562cc3520251a8fe0215e22b416df98f3a8
ReynaldyNC <reynaldy.nur.cahyo.003@gmail.com>
2022-12-20 11:52:32 +0700

    New - Decoder - Command and Argument for ease of use
    Changes
    - Optimizations

--- main.cpp
+++ main.cpp
@@ -1,32 +1,51 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <math.h>;
+#include <math.h>
+#include <chrono>
 
-void encode() {
-	// Declaration
+// Base64 Alphabet code function
+char base_alphabet(int index) {
 	char alphabet[] = { 'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
 		'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-		'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/'};
+		'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/' };
 
-	std::string text, bits;
+	return alphabet[index];
+}
+
+// Base64 Alphabet char function
+int base_code(char chr) {
+	if (chr == '=') {
+		return NULL;
+	}
+
+	char alphabet[] = { 'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+		'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+		'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/' };
+
+	int len = sizeof(alphabet) / sizeof(char);
+
+	for (int i = 0; i < len; i++) {
+		if (chr == alphabet[i]) {
+			return i;
+		}
+	}
+}
+
+std::string encode(std::string text) {
+	// Declaration
 	std::vector<int> binary;
-	std::vector<std::string> binaryText;
 
-	// User input
-	std::cout << "Input a text: "; std::getline(std::cin, text);
-
-	// Get the ASCII code for each letter and put it inside octets vector
+	// Get the ASCII code for each letter and put it inside binary vector
 	for (int i = 0; i < text.length(); i++) {
 		binary.push_back(int(text[i]));
 	}
 
-	//std::cout << std::endl;
-
 	// Turn ASCII code into binary
+	std::string bits;
 	for (int i = 0; i < binary.size(); i++) {
 		// Declaration
-		std::string temp, revTemp;
+		std::string revTemp;
 		int bit = binary[i];
 
 		// While the ASCII is bigger than 0
@@ -35,27 +54,28 @@ void encode() {
 			int x = bit % 2;
 
 			// Put it inside a string that we already declared
-			temp += std::to_string(x);
+			revTemp += std::to_string(x);
 
 			// We divide the ASCII until it reaches zero
 			bit /= 2;
 		}
 
 		// If the binary number length is less than 8, we put a 0 behind
-		while (temp.length() < 8) {
-			temp += "0";
+		while (revTemp.length() < 8) {
+			revTemp += "0";
 		}
 
 		// We reversed the binary code, because the order is incorrect when we insert it
-		for (int j = temp.length() - 1; j >= 0; j--) {
-			revTemp += temp[j];
+		for (int j = revTemp.length() - 1; j >= 0; j--) {
+			bits += revTemp[j];
 		}
-
-		bits += revTemp;
 	}
 
 	// We clear the vector so we can use it later
 	binary.clear();
+
+	// Declare a vector to store the whole binary string
+	std::vector<std::string> binaryText;
 
 	// We divide the binaries and put it into four group of 6 elements
 	while (bits.length() > 0) {
@@ -68,7 +88,7 @@ void encode() {
 		std::string temp = bits.substr(0, 6);
 		// Insert it into a vector
 		binaryText.push_back(temp);
-		// We make it simple to take 6 element without having to change the substr
+		// We make it simple to take 6 element without having to change the substr by erasing first six element
 		bits.erase(0, 6);
 	}
 
@@ -94,8 +114,8 @@ void encode() {
 	std::string output;
 	// Last but not least, we combine all Base64 alphabet code into a readable words.
 	for (int i = 0; i < binary.size(); i++) {
-		// We call the char array that already have been declared and match it with the order.
-		output += alphabet[binary[i]];
+		// We call the base64_alphabet function that already have been declared.
+		output += base_alphabet(binary[i]);
 	}
 
 	// Declaration
@@ -111,14 +131,103 @@ void encode() {
 		padding--;
 	}
 
-	// Now we print the output
-	std::cout << output;
+	// Now we return the output
+	return output;
 }
 
-void decode() {}
+std::string decode(std::string text) {
+	// Declaration
+	std::vector<int> bits;
+
+	// Get each character Base64 code and put it in bits vector
+	for (int i = 0; i < text.length(); i++) {
+		bits.push_back(base_code(text[i]));
+	}
+
+	// Turn Base64 code into binary
+	std::string binaryString;
+	for (int i = 0; i < bits.size(); i++) {
+		std::string revString;
+
+		// Calculate Binary
+		while (bits[i] > 0) {
+			int remain = bits[i] % 2;
+			revString += std::to_string(remain);
+			bits[i] /= 2;
+		}
+
+		// If Binary length is less than 6, put a 0 behind it
+		while (revString.length() < 6) {
+			revString += "0";
+		}
+
+		// Reverse the string so it would match before we divide it to a group of 8
+		for (int j = revString.length() - 1; j >= 0; j--) {
+			binaryString += revString[j];
+		}
+	}
+
+	// Clear bits vector so we could use it later
+	bits.clear();
+
+	// Divide the binary into a group with 8 elements
+	std::vector<std::string> binaryTemp;
+	while (binaryString.length() > 0) {
+		std::string temp = binaryString.substr(0, 8);
+		binaryTemp.push_back(temp);
+		binaryString.erase(0, 8);
+	}
+
+	// Convert from Binary to ASCII code
+	for (int i = 0; i < binaryTemp.size(); i++) {
+		int result = 0;
+		for (int j = binaryTemp[i].length() - 1; j >= 0; j--) {
+			if (binaryTemp[i].at(j) % 2 == 1) {
+				result += pow(2, binaryTemp[i].length() - 1 - j);
+			}
+		}
+
+		// Put the ASCII into the bits vector
+		bits.push_back(result);
+	}
+
+	// We convert the ASCII to char
+	std::string output;
+	for (int i = 0; i < bits.size(); i++) {
+		output += char(bits[i]);
+	}
+
+	// Return the output
+	return output;
+}
 
 int main() {
-	encode();
+	// Declaration
+	std::string input;
+	
+	while (true) {
+		// User input
+		std::cout << ">> "; std::getline(std::cin, input);
+
+		// Command and argument
+		std::size_t blank = input.find(" ");
+		std::string command = input.substr(0, blank);
+		std::string arg = input.substr(blank + 1, input.length());
+
+		if (command == "encode") {
+			std::cout << ">> " << encode(arg) << std::endl;
+		} else if (command == "decode") {
+			std::cout << ">> " << decode(arg) << std::endl;
+		} else if (command == "exit"){
+			return 0;
+		} else {
+			std::cout << "\nInvalid Command\n";
+		}
+
+		// Pause and close
+		system("pause");
+		system("cls");
+	}
 
 	return 0;
 }
